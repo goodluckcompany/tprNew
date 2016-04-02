e4ca24141de579b707cd3bcbe98d835c953ca624
sl0nWorker <battousai45736@gmail.com>
2016-04-02 22:23:24 +0400

    additional probability

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -24,6 +24,7 @@ Answer * MainWindow::calculateBayesianDecision(Alternative **_alternatives)
     double u1,u2; //Функция полезности при принятии решения d1 и d2 соответствено;
     u1 = alt[0]->getProbability()*alt[0]->getValuation()+alt[1]->getProbability()*alt[1]->getValuation();
     u2 = alt[2]->getProbability()*alt[2]->getValuation()+alt[3]->getProbability()*alt[3]->getValuation();
+
     if(u1 >= u2)
     {
         return new Answer(u1,alt[0]->getDecision());
@@ -75,10 +76,37 @@ Answer * MainWindow::calculateUsefulnessAccurateInformation(Alternative **_alter
 
 void MainWindow::calculate()
 {
+
     /*Массива вероятностей события*/
-    double probability[2];
-      probability[0]  =  ui->doubleSpinBox->value(); // вероятность успеха  товара
-      probability[1] = 1 - probability[0];
+
+    double probability[4];
+    probability[0]  =  ui->doubleSpinBox->value(); // вероятность успеха простого товара
+    probability[1] = 1 - probability[0];
+    probability[2] = ui->doubleSpinBox_2->value(); // вероятность успеха иновационного товара
+    probability[3] = 1 - probability[2];
+
+    /*Переменная для учота дополнительный рисков*/
+     double addprob = 1;
+
+    if(ui->doubleSpinBox_7->isEnabled())
+        addprob = addprob * ui->doubleSpinBox_7->value();
+    if (ui->doubleSpinBox_8->isEnabled())
+        addprob = addprob * ui->doubleSpinBox_8->value();
+    if (ui->doubleSpinBox_9->isEnabled())
+        addprob = addprob * ui->doubleSpinBox_9->value();
+
+    if (addprob != 1)
+    {
+
+     probability[2] = probability[2] - addprob;
+     probability[3] = 1 - probability[3];
+     if (probability[2]<0)
+     {
+         probability[2] = 0;
+         probability[3] = 1;
+     }
+    }
+
 
     /*Массив возможных решений*/
     QString decisions[2];
@@ -102,10 +130,10 @@ void MainWindow::calculate()
     alernatives[1] = new Alternative(decisions[0],probability[1],valuation[1]);
 
     //успешное вложение в иновационный товар
-    alernatives[2] = new Alternative(decisions[1],probability[0],valuation[2]);
+    alernatives[2] = new Alternative(decisions[1],probability[2],valuation[2]);
 
     //неуспешное вложение в иновационный товар
-    alernatives[3] = new Alternative(decisions[1],probability[1],valuation[3]);
+    alernatives[3] = new Alternative(decisions[1],probability[3],valuation[3]);
 
 
     //Произодим расчет Байесовского решения
@@ -120,5 +148,6 @@ void MainWindow::calculate()
     ui->plainTextEdit->appendPlainText(trUtf8("Полезность решения: ")+QString::number(BayesianDecision->getUsefulness()));
     ui->plainTextEdit->appendPlainText(trUtf8("Было принято решение: ")+MinMaxDecision->getDecision());
     ui->plainTextEdit->appendPlainText(trUtf8("Полезность решения: ")+QString::number(MinMaxDecision->getUsefulness()));
-    ui->plainTextEdit->appendPlainText(QString::number(UsefulnessAccurateInformation->getUsefulness()));
+    ui->plainTextEdit->appendPlainText(trUtf8("Полезность точной информации: ")+QString::number(UsefulnessAccurateInformation->getUsefulness()));
+
 }
